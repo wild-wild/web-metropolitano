c76db83abaf2389e9a9b508cea4d11dae39fc73a
wild <wild.addsystem@gmail.com>
2024-09-26 11:36:00 -0400

    add
    add

--- resources/views/componentes/head.blade.php
+++ resources/views/componentes/head.blade.php
@@ -7,7 +7,7 @@
     <!--Bootstrap CSS-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     {{-- estilos propiso --}}
-    <link rel="stylesheet" href="{{ asset('css/diseño.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/estilos.css') }}">
     <!-- iconos fontaweasome -->
     <link href="{{ asset('iconos/fontawesome-free-6.1.1-web/css/all.css') }}" rel="stylesheet">
     <!--titulo-->
